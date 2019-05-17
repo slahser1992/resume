7d2af87b37a577a93a3d38e32b44ce3ac7756455
Slahser Zou <284572765@qq.com>
2019-05-17 16:33:46 +0800

    Updates

---
+++ precache-manifest.b03f3cdf6f32bb4255f0a8dce61b172b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5e9f01c4e1afb1d194f42f7ce3282e",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "ee00790e10cd1d6fd162",
+    "url": "/resume/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/resume/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ee00790e10cd1d6fd162",
+    "url": "/resume/static/js/main.e0ff1e8d.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
